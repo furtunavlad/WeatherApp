0f5a73844d239aa313a108c185a6cb52b22a4f10
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-02-29 19:58:04 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Vlad Furtuna on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
