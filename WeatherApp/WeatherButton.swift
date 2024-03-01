46827ab9e80a393cd71dddd8f3ce08d1448ca7c8
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-03-02 00:13:29 +0200

    initial commit

---
+++ WeatherApp/WeatherButton.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherButton.swift
+//  WeatherApp
+//
+//  Created by Vlad Furtuna on 01.03.2024.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
+    }
+}
